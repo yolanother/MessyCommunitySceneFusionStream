d404172be7502e8cf396761b71b6f78c03e86521
Yolan <yolanother@gmail.com>
2021-05-01 09:24:38 -0700

    Create symlink for creating a local project instance from a dropbox dir

---
+++ symlink.bat
@@ -0,0 +1,18 @@
+@REM Use this to symlink your project from dropbox
+@REM Run this in an empty directory you want to use for your new project.
+@REM You will need to update the root path to your dropbox dir for this to work.
+
+mklink KinematicSoup /J d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\KinematicSoup
+mklink Packages /J d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Packages
+mklink ProjectSettings /J d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\ProjectSettings
+mklink assetManifest.v2.json d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\assetManifest.v2.json
+mkdir Assets
+cd Assets
+mklink /J _Project d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\_Project
+mklink /J DoubTech d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\DoubTech
+mklink /J KinematicSoup d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\KinematicSoup
+mklink /J PolygonFantasyKingdom d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\PolygonFantasyKingdom
+mklink PolygonFantasyKingdom.meta d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\PolygonFantasyKingdom.meta
+mklink KinematicSoup.meta d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\KinematicSoup.meta
+mklink DoubTech.meta d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\DoubTech.meta
+mklink _Project.meta d:\Dropbox\Unity\projects\MessyCommunity\SceneFusionStream\Assets\_Project.meta
